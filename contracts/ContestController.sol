120d0dec87a2e3c05cd900c37b763bbdfe8dcd93
Guillem Cordoba <guillem.cordoba@gmail.com>
2018-09-14 02:14:26 +0200

    Fixed height

--- contracts/ContestController.sol
+++ contracts/ContestController.sol
@@ -98,7 +98,18 @@ contract ContestController is owned {
     mapping (bytes32 => bool) public existingTags;
     bytes32[] public tagsList;
 
+  /*   // ONLY FOR TESTING PURPOSES
+    // FIXME: SECURITY ERROR
+    uint date = 0;
+    function setTime (uint256 newDate) public onlyOwner {
+        date = newDate;
+    }
 
+    function getTime() public view returns (uint256){
+        if (date == 0)
+          return now;
+        else return date;
+    } */
     function getTime() public view returns (uint256){
         return now;
     }
