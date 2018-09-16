3fa8ca640b7c803973d0d64c595deca1307c02d0
guillem <guillem.cordoba@gmail.com>
2018-09-17 00:18:19 +0200

    Production ready application

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  contractAddress: '0x7af5a57df4c63ca9b9fb0fd195ca9f4f3c91b52e'
+  contractAddress: '0x7812828924134379cadbbaaf1701f7390993c121'
 };
--- truffle.js
+++ truffle.js
@@ -1,10 +1,18 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+
+var infura_apikey = "WzQrijeM6mNgaf0TAH5z";
+var mnemonic = "student tent patch puzzle away swing cake toe surprise same purchase range";
+
 module.exports = {
-  networks:{
-    ganache:{
-      host: "127.0.0.1",
+  networks: {
+    development: {
+      host: "localhost",
       port: 8545,
-      network_id: "*",
+      network_id: "*" // Match any network id
+    },
+    ropsten: {
+      provider: new HDWalletProvider(mnemonic, "https://ropsten.infura.io/"+infura_apikey),
+      network_id: 3
     }
   }
- };
- 
+};
